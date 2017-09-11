2bab39a4308a3c5380b7fa8fd0732399200b2c2d
liuqi <liuqi0315@gmail.com>
2017-09-11 14:44:59 +0800

    feature: add c# client

---
+++ csharp_client/csharp_client/Config.cs
@@ -0,0 +1,21 @@
+﻿/* 在该文件中设置：
+ *		服务器 IP 地址
+ *		服务器端口号
+ * 引用：
+ *		Config.SERVER_IP
+ *		Config.SERVER_PORT
+ */
+using System;
+
+namespace csharp_client
+{
+	class Config
+	{
+        // 部署用的服务器 IP 地址
+        // public const String SERVER_IP = "111.231.1.210";
+
+        // 测试用的本机 IP 地址
+        public const String SERVER_IP = "192.168.1.165";
+        public const int SERVER_PORT = 21567;
+	}
+}
---
+++ csharp_client/csharp_client/LoginWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace csharp_client
+{
+	/// <summary>
+	/// LoginWindow.xaml 的交互逻辑
+	/// </summary>
+	public partial class LoginWindow : Window
+	{
+		public LoginWindow()
+		{
+			InitializeComponent();
+			username.Focus();
+			// 设置用户名输入框为焦点
+		}
+
+		private void login_Click(object sender, RoutedEventArgs e)
+		{
+			// 点击登录按钮事件
+			Button button = sender as Button;
+			string input_username = username.Text;
+			string receive = null;
+
+			// 在本地处理不规范的 username
+			try
+			{
+				int.Parse(input_username);
+				//MessageBox.Show(i.ToString());
+			}
+			catch (FormatException)
+			{
+				// 通过类型转换来判断用户名是否非法
+				MessageBox.Show("用户名不能包含非法字符（数字以外的字符）！", "Error");
+				username.Text = "";
+				username.Focus();
+				return;
+			}
+			catch (Exception exp)
+			{
+				// 捕捉未处理的异常
+				MessageBox.Show(exp.ToString(), "Exception");
+				return;
+			}
+
+
+			// 处理登录事件
+			if (button.Name.Equals("login"))
+			{
+				SocketClient socketClient = new SocketClient();
+				receive = socketClient.Send("csharp:" + input_username);
+				// 对要发送的消息进行修饰，便于服务器的识别
+
+				if (receive.Equals("success"))
+				{
+					MessageBox.Show("登录成功！", "提示");
+					MainWindow mainWindow = new MainWindow
+					{
+						username = input_username
+					};
+					mainWindow.Show();
+					Close();
+				}
+				else if (receive.Equals("failed"))
+				{
+					MessageBox.Show("用户名未注册！", "Error");
+					username.Text = "";
+					username.Focus();
+				}
+				else
+				{
+					//MessageBox.Show("Connection Exception", "Error");
+					username.Text = "";
+					username.Focus();
+				}
+			}
+		}
+	}
+}
