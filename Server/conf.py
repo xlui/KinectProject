042a0fb7673da11f6c2bcbf50866754a8fac6851
liuqi <liuqi0315@gmail.com>
2017-09-11 15:37:02 +0800

    refactor: update jpush app key and master key

--- Server/conf.py
+++ Server/conf.py
@@ -7,5 +7,5 @@ table = 'account'
 username = 'kinect'
 password = 'kinect'
 # variables for push.py
-app_key = u'bb46127fd9ff41bf4d8f5bec'
-master_secret = u'8934d8ec72156d2f3dc4e785'
+app_key = u'01d93632e5886f1145431c1e'
+master_secret = u'6a7f9e8c5c248d00b401dca6'
