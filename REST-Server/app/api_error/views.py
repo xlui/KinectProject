d1f8662ed755b21fe21c7bd3c79a25c2a24e6d04
xlui <liuqi0315@gmail.com>
2017-10-03 18:45:12 +0800

    Feature: bind a index page in error api

--- REST-Server/app/api_error/views.py
+++ REST-Server/app/api_error/views.py
@@ -6,3 +6,8 @@ from . import api
 @api.app_errorhandler(404)
 def page_not_found(error):
     return make_response(jsonify({'error':'Not Found'}), 404)
+
+
+@api.route('/')
+def index():
+    return '<div align="center"><h1>Hello World</h1></div>'
