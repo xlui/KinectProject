7f4b960c703615d3203d42ad0def3cdee583f0a5
xlui <liuqi0315@gmail.com>
2017-10-08 13:14:08 +0800

    Feature: C# client for rest-server

---
+++ csharp_client/csharp_client/Client.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace csharp_client
+{
+    class Client
+    {
+        public void Get(String url, String username, String password)
+        {
+            String username_password = username + ":" + password;
+            // user authorization
+            String base64_username_password = Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(username_password));
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.Headers.Add(HttpRequestHeader.Authorization, "Basic " + base64_username_password);
+            // request
+            HttpWebResponse response = null;
+            // response
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+            } catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return;
+            }
+            int response_length = (int)response.ContentLength;
+            byte[] response_byte = new Byte[response.ContentLength];
+            Stream stream = response.GetResponseStream();
+
+            try
+            {
+                stream.Read(response_byte, 0, (int)response.ContentLength);
+                String response_string = System.Text.Encoding.UTF8.GetString(response_byte, 0, response_length).ToString();
+                Console.WriteLine(response_string);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+        }
+
+        public String Post()
+        {
+            return "";
+        }
+    }
+}
---
+++ csharp_client/csharp_client/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace csharp_client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            string url = "http://111.231.1.210/api/dev/";
+            string username = "1", password = "dev";
+            client.Get(url, username, password);
+        }
+    }
+}
