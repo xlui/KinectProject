63a6720fc914cbaf7b5ea7af6a178ae7e166b603
xlui <liuqi0315@gmail.com>
2017-10-08 15:06:43 +0800

    Feature: http POST method available

--- REST-Server/app/api_dev/views.py
+++ REST-Server/app/api_dev/views.py
@@ -1,4 +1,4 @@
-from flask import jsonify
+from flask import jsonify, request, abort
 from sqlalchemy import func
 
 from . import api
@@ -6,9 +6,20 @@ from .. import db
 from ..models import State
 
 
-@api.route('/')
+@api.route('/', methods=['GET'])
 def index():
     """Will only show the latest hand state"""
     latest = db.session.query(func.max(State.id)).first()[0]
     state = State.query.get(latest)
+    return jsonify({'state': state.get_json()})
+
+
+@api.route('/update', methods=['POST'])
+def update():
+    """update latest hand state"""
+    # if not request.json or not 'state' in request.json:
+    #     abort(400)
+    state = State(state=request.json.get('state'))
+    db.session.add(state)
+    db.session.commit()
     return jsonify({'state': state.get_json()})
--- REST-Server/app/api_error/views.py
+++ REST-Server/app/api_error/views.py
@@ -3,9 +3,14 @@ from flask import make_response, jsonify
 from . import api
 
 
+@api.app_errorhandler(400)
+def bad_request(error):
+    return make_response(jsonify({'error': 'Bad request!'}), 400)
+
+
 @api.app_errorhandler(404)
 def page_not_found(error):
-    return make_response(jsonify({'error':'Not Found'}), 404)
+    return make_response(jsonify({'error': 'Not Found'}), 404)
 
 
 @api.route('/')
