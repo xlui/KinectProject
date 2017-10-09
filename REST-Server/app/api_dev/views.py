be5420d2b0257631dcd1c2b57076073684dd4cd5
xlui <liuqi0315@gmail.com>
2017-10-09 16:44:38 +0800

    Feature: client register

--- REST-Server/app/api_dev/views.py
+++ REST-Server/app/api_dev/views.py
@@ -2,17 +2,32 @@ from flask import jsonify, request, abort, make_response
 from sqlalchemy import func
 
 from . import api
+from .authentication import multi_auth
 from .. import db
-from ..models import State
+from ..models import User, State
 
 
 @api.route('/login', methods=['GET'])
+@multi_auth.login_required
 def login():
     """client login"""
     return make_response(jsonify({'login': 'success'}), 200)
 
 
+@api.route('/register', methods=['POST'])
+def register():
+    """user register"""
+    if not request.json or not 'username' in request.json:
+        abort(400)
+    user = User(username=request.json.get('username'),
+                password=request.json.get('password'))
+    db.session.add(user)
+    db.session.commit()
+    return make_response(jsonify({'register': 'success'}), 200)
+
+
 @api.route('/latest', methods=['GET'])
+@multi_auth.login_required
 def index():
     """Will only show the latest hand state"""
     latest = db.session.query(func.max(State.id)).first()[0]
@@ -21,6 +36,7 @@ def index():
 
 
 @api.route('/update', methods=['POST'])
+@multi_auth.login_required
 def update():
     """update latest hand state"""
     if not request.json or not 'state' in request.json:
