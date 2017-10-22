b89cda6df58da4787d31b670e7582e3311cfeba9
xlui <liuqi0315@gmail.com>
2017-10-22 21:59:17 +0800

    change picture request response url and track db file

--- REST-Server/app/api_dev/views.py
+++ REST-Server/app/api_dev/views.py
@@ -30,7 +30,7 @@ def register():
 @multi_auth.login_required
 def picture(name):
     root_url = 'https://nxmup.com'
-    return redirect(root_url + url_for('static', filename='images/test.png'))
+    return root_url + url_for('static', filename='images/test.png')
 
 
 @api.route('/latest', methods=['GET'])
