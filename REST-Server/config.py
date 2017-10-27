c4bdb47b103db060690195683ba06af69d7140af
liuqi <liuqi0315@gmail.com>
2017-10-27 20:00:17 +0800

    server: ignore database file

--- REST-Server/config.py
+++ REST-Server/config.py
@@ -7,7 +7,7 @@ class Config:
     SECRET_KEY = "This is secret key to use SCRF, must be hard to guess"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/dev.sqlite' or 'sqlite:///' + os.path.join(basedir, 'dev.sqlite')
 
     @staticmethod
     def init_app(app):
