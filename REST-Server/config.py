190d92e955092c573228f6d0dd921af462a6eb90
liuqi <liuqi0315@gmail.com>
2017-10-30 20:28:40 +0800

    fix: command show works well now

--- REST-Server/config.py
+++ REST-Server/config.py
@@ -11,7 +11,7 @@ class Config:
     SECRET_KEY = "This is secret key to use SCRF, must be hard to guess"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + windows_database if 'Windows' in platform.platform() else linux_database
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + (windows_database if 'Windows' in platform.platform() else linux_database)
 
     @staticmethod
     def init_app(app):
